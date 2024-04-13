0f0c02eaf7951cf00187a640dd05804794dc1c24
vinod0505 <vinodbojaraju@gmail.com>
2024-04-13 12:20:43 +0530

    project created , added Header

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <h1 className='font-bold bg-black text-5xl text-center text-white'>My Todo List</h1>
+    </>
+  )
+}
+
+export default App
